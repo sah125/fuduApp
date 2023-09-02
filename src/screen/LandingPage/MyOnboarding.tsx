47e33dde6632c3e8c56b0ae642b04f877a42b05f
sah125 <sah.sakhile@gmail.com>
2023-09-02 05:38:41 +0200

    Onbording position

--- src/screen/LandingPage/MyOnboarding.tsx
+++ src/screen/LandingPage/MyOnboarding.tsx
@@ -1,272 +1,160 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
-import { StackNavigationProp } from '@react-navigation/stack'; 
-import { RootStackParamList } from '../AppNavigator'; 
+import React from "react";
+import { View, Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar, Button } from "react-native";
+import Swiper from 'react-native-swiper-flatlist';
+import { useNavigation } from "@react-navigation/native"; // Import useNavigation
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
-type MyOnboardingNavigationProp = StackNavigationProp<RootStackParamList, 'Onboarding'>;
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
- // Replace YourStackParamList with your actual type
-
-interface MyOnboardingProps {
-  navigation: MyOnboardingNavigationProp;
-}
-
-const MyOnboarding: React.FC<MyOnboardingProps> = ({ navigation }) => (
-  <Onboarding
-    titleStyles={{ color: '#03a60e' }}
-    pages={[
-      {
-        backgroundColor: 'red',
-        title: (
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>
-              Find food{' '}
-              <Text style={styles.boldText}>Restaurants Near</Text> you
-            </Text>
-            <Text style={styles.subtitleText}>
-              Contrary to popular belief borem text. It has roots clintock
-            </Text>
-          </View>
-        ),
-        subtitle: '',
-        imageContainerStyles: styles.slideContainerVertical,
-        image: (
-          <View style={styles.slideContainerVertical}>
-            <View style={styles.half}>
-              <View style={styles.content}>
-                <Image source={require('../LandingPage/bgImage.png')} />
-                <View style={styles.imgContainer}>
+const MyOnboarding = () => {
+  const navigation = useNavigation();
+  return (
+    <Swiper style={styles.wrapper} >
+        <View style={styles.slide}>
+          {/* Content for slide 1 */}
+          <View style={styles.container}>
+            <View style={styles.topImagesContainer}>
+              
+                <ImageBackground
+                  source={require('../../../assets/images/bgImage.png')}
+                  style={styles.bgImage}
+                >
+                  <Image source={require('../../../assets/images/pins.png')} style={styles.pinsImage} />
+                </ImageBackground>
+              <View style={styles.centeredTopContainer}>
+                <View style={styles.bottomImageContainer}>
                   <View style={styles.whiteCircle}>
-                    <View style={styles.imageRow}>
-                      <Image source={require('../LandingPage/pin.png')} style={styles.iconImage} />
-                      <Image source={require('../LandingPage/burger.png')} style={styles.image} />
-                    </View>
+                    <Image source={require('../../../assets/images/pin.png')} style={styles.imagePin} />
+                    <Image source={require('../../../assets/images/burger.png')} style={styles.centerImage} />
                   </View>
                 </View>
               </View>
-              <View style={styles.bottomImageContainer}>
-                <Image source={require('../LandingPage/cake.png')} style={styles.bottomImage} />
-              </View>
-            </View>
-            <View style={styles.topImageContainer}>
-              <Image source={require('../LandingPage/pins.png')} style={styles.topImage} />
-            </View>
-          </View>
-        ),
-      },
-      {
-        backgroundColor: 'red',
-        title: (
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>
-              <Text style={styles.boldText}>Food</Text> you love delivered {' '} 
-              <Text style={styles.boldText}>24hr to you</Text>
-            </Text>
-            <Text style={styles.subtitleText}>
-              Contrary to popular belief borem text. It has roots clintock
-            </Text>
-          </View>
-        ),
-        subtitle: '',
-        imageContainerStyles: styles.slideContainerVertical,
-        image: (
-          <View style={styles.slideContainerVertical}>
-            <View style={styles.half}>
-              <View style={styles.content}>
-                <Image source={require('../LandingPage/bgImage2.png')} />
-                <View style={styles.imgContainer}>
-                  <View style={styles.orangeCircle}>
-                    <View style={styles.imageRow}>
-                      <Image source={require('../LandingPage/burger.png')} style={styles.image} />
-                    </View>
-                  </View>
-                </View>
-              </View>
-              <View style={styles.bottomImageContainer}>
-                <Image source={require('../LandingPage/burgerImg.png')} style={styles.bottomImage} />
-              </View>
-            </View>
-          </View>
-        ),
-      },
-      {
-        backgroundColor: 'red',
-        title: (
-          <View style={styles.titleContainerBike}>
-            <Text style={styles.titleText}>
-              <Text style={styles.boldText}>Delivered quickly</Text> at your {' '} 
-              <Text style={styles.boldText}>place</Text>
-            </Text>
-            <Text style={styles.subtitleText}>
-              Contrary to popular belief borem text. It has roots clintock
-            </Text>
-          </View>
-        ),
-        subtitle: '',
-        imageContainerStyles: styles.slideContainerVertical,
-        image: (
-          <View style={styles.slideContainerVertical}>
-            <View style={styles.flatContainer}>
-              <View style={styles.flat}>
-                <Image source={require('../LandingPage/flat.png')} />
-                <View style={styles.imgContainer}>
-                  <View style={styles.bikeimageRow}>
-                    <Image source={require('../LandingPage/bike.png')} style={styles.imageBike} />
-                  </View>
-                </View>
-              </View>
-             
-            </View>
-            <TouchableOpacity
-      onPress={() => navigation.navigate('LandingPage')}
-      style={styles.bottomImageContainerCake}
-    >
-      <Image source={require('../LandingPage/cupcake.png')} style={styles.bottomImageCake} />
-    </TouchableOpacity>
-          </View>
-        ),
-      },
-    ]}
-  />
-);
+      
+      <View style={styles.textContainer}>
+        <Text style={styles.titleText}>
+          Find food{' '}
+          <Text style={styles.boldText}>Restaurants Near</Text> you
+        </Text>
+        <Text style={styles.subTitleText}>
+          Contrary to popular belief, Lorem Ipsum is not simply random text.
+        </Text>
+      </View>
+      </View>
+      <Image source={require('../../../assets/images/cake.png')} style={styles.footerImage} />
+    </View>
+      </View>
+      <View style={styles.slide}>
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate("Home")}
+        />
+      </View>
+    </Swiper>
+  );
+};
 
 const styles = StyleSheet.create({
-  slideContainerVertical: {
+  wrapper: {
     flex: 1,
-    width: windowWidth,
-    height: windowHeight,
+    backgroundColor: "#D90504",
+  },
+  slide: {
+  
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D90504",
+  },
+  container: {
+  
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D90504",
+  },
+  topImagesContainer: {
+    position: 'relative',
+    flex: 1, // Occupy the entire available space
+  
+  },
+  centeredTopContainer: {
+    justifyContent: 'flex-start', 
+    alignItems: 'center',
+  },
+  bgImage: {
+    width: windowWidth, 
+    height: windowHeight * 0.51,
+    // Cover the image
+  },
+  centeredBottomContainer: {
+    flex: 1,
+    
+    alignItems: 'center',
+  },
+  centerImage: {
+    width: windowWidth * 0.15,
+    height: windowWidth * 0.15,
     resizeMode: 'contain',
   },
-  half: {
-    width: windowWidth,
-    height: windowHeight / 6,
-    paddingVertical: windowHeight * 0.2,
-    marginTop: -windowHeight * 0.8,
+  bottomImageContainer: {
+    position: 'absolute',
+    bottom: -20,
+   
   },
-  flatContainer: {
-    width: windowWidth,
-    height: windowHeight,
-    paddingVertical: windowHeight * 0.3,
-    marginTop: -windowHeight * 0.5,
-    paddingBottom: -windowHeight,
-  },
-  content: {
-    alignItems: 'center',
-  },
-  flat: {
-    alignItems: 'center',
-  },
-  imgContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: -windowHeight * 0.15,
+  pinsImage: {
+    position: 'absolute',
+    top: 50,
+    right: 30,
+    width: windowWidth * 0.3,
+    height: windowWidth * 0.3,
+    resizeMode: 'contain',
     zIndex: 1,
   },
   whiteCircle: {
-    width: windowHeight * 0.18,
-    height: windowHeight * 0.18,
-    borderRadius: windowHeight * 0.095,
+    width: windowWidth * 0.35,
+    height: windowWidth * 0.35,
+    borderRadius: windowWidth * 0.2,
     backgroundColor: '#FFF',
     justifyContent: 'center',
     alignItems: 'center',
+    right: 60,
   },
-  orangeCircle: {
-    width: windowHeight * 0.18,
-    height: windowHeight * 0.18,
-    borderRadius: windowHeight * 0.095,
-    backgroundColor: '#D97803',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: 50,
-    height: 50,
+  imagePin: {
+    width: windowWidth * 0.15,
+    height: windowWidth * 0.15,
+    marginRight: -windowWidth * 0.15,
     resizeMode: 'contain',
+    marginBottom: -20,
+    
   },
-  imageBike: {
-    width: 100,
-    height: 100,
+  footerImage: {
+    width: windowWidth * 0.3,
+    height: windowWidth * 0.3,
     resizeMode: 'contain',
-    
-    
+    position: 'relative',
+    left: 60, 
+  },
+  textContainer: {
+   
+    position: 'relative',
+    right: 40,
+    margin: 20,
+    padding: 20,
+    paddingTop: 10, 
+  },
+
+  titleText: {
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'left',
   },
   boldText: {
     fontWeight: 'bold',
   },
-  titleContainer: {
-    alignItems: 'center',
-    marginTop: windowHeight * 0.09,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  titleContainerBike: {
-    marginTop: windowHeight * 0.2,
-    paddingLeft: 20,
-    paddingRight: 40,
-  },
-  titleText: {
-    fontSize: 30,
-    marginBottom: 10,
+  subTitleText: {
+    fontSize: 16,
     color: 'white',
     textAlign: 'left',
-  },
-  subtitleText: {
-    fontSize: 17,
-    color: 'white',
-    textAlign: 'left',
-  },
-  imageRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  bikeimageRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: windowHeight * 0.04,
-  },
-  iconImage: {
-    width: 50,
-    height: 50,
-    resizeMode: 'contain',
-    marginRight: -80,
-    marginBottom: 30,
-  },
-  bottomImageContainer: {
-    alignItems: 'center',
-    marginBottom: -windowHeight * 0.5,
-    marginLeft: windowHeight * 0.3,
-  },
-  bottomImageContainerCake: {
-    alignItems: 'flex-end',
-    marginLeft: windowHeight *0.55,
-    marginTop: -windowHeight * 0.09,
-
-  },
-
-  bottomImageCake: {
-    width: 175,
-    height: 190,
-    resizeMode: 'contain',
-  },
-  bottomImage: {
-    width: 150,
-    height: 1650,
-    resizeMode: 'contain',
-  },
-  
-  topImageContainer: {
-    alignItems: 'center',
-    marginTop: windowHeight * 0.15,
-    marginLeft: windowWidth * 0.45,
-  },
-  topImage: {
-    width: 120,
-    height: 120,
-    resizeMode: 'contain',
+    paddingTop: 10, 
+   
   },
 });
 
