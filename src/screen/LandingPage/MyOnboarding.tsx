70b3291ccb8ce91b57fad0bf52864ba3f8f89df1
seraphin <sngoymunga733@gmail.com>
2023-09-02 06:53:17 +0200

    fix:pading-on-onboarding

--- src/screen/LandingPage/MyOnboarding.tsx
+++ src/screen/LandingPage/MyOnboarding.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar, Button } from "react-native";
-import Swiper from 'react-native-swiper-flatlist';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  StatusBar,
+  Button,
+} from "react-native";
+import Swiper from "react-native-swiper-flatlist";
 import { useNavigation } from "@react-navigation/native"; // Import useNavigation
-
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -12,41 +20,52 @@ interface HomePageProps {
 }
 
 const MyOnboarding: React.FC<HomePageProps> = ({ navigation }) => {
- 
   return (
-    <Swiper style={styles.wrapper} >
-        <View style={styles.slide}>
-          {/* Content for slide 1 */}
-          <View style={styles.container}>
-            <View style={styles.topImagesContainer}>
-              
-                <ImageBackground
-                  source={require('../../../assets/images/bgImage.png')}
-                  style={styles.bgImage}
-                >
-                  <Image source={require('../../../assets/images/pins.png')} style={styles.pinsImage} />
-                </ImageBackground>
-              <View style={styles.centeredTopContainer}>
-                <View style={styles.bottomImageContainer}>
-                  <View style={styles.whiteCircle}>
-                    <Image source={require('../../../assets/images/pin.png')} style={styles.imagePin} />
-                    <Image source={require('../../../assets/images/burger.png')} style={styles.centerImage} />
-                  </View>
+    <Swiper style={styles.wrapper}>
+      <View style={styles.slide}>
+        {/* Content for slide 1 */}
+        <View style={styles.container}>
+          <View style={styles.topImagesContainer}>
+            <ImageBackground
+              source={require("../../../assets/images/bgImage.png")}
+              style={styles.bgImage}
+            >
+              <Image
+                source={require("../../../assets/images/pins.png")}
+                style={styles.pinsImage}
+              />
+            </ImageBackground>
+            <View style={styles.centeredTopContainer}>
+              <View style={styles.bottomImageContainer}>
+                <View style={styles.whiteCircle}>
+                  <Image
+                    source={require("../../../assets/images/pin.png")}
+                    style={styles.imagePin}
+                  />
+                  <Image
+                    source={require("../../../assets/images/burger.png")}
+                    style={styles.centerImage}
+                  />
                 </View>
               </View>
-      
-      <View style={styles.textContainer}>
-        <Text style={styles.titleText}>
-          Find food{' '}
-          <Text style={styles.boldText}>Restaurants Near</Text> you
-        </Text>
-        <Text style={styles.subTitleText}>
-          Contrary to popular belief, Lorem Ipsum is not simply random text.
-        </Text>
-      </View>
-      </View>
-      <Image source={require('../../../assets/images/cake.png')} style={styles.footerImage} />
-    </View>
+            </View>
+
+            <View style={styles.textContainer}>
+              <Text style={styles.titleText}>
+                Find food <Text style={styles.boldText}>Restaurants Near</Text>{" "}
+                you
+              </Text>
+              <Text style={styles.subTitleText}>
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text.
+              </Text>
+            </View>
+          </View>
+          <Image
+            source={require("../../../assets/images/cake.png")}
+            style={styles.footerImage}
+          />
+        </View>
       </View>
       <View style={styles.slide}>
         <Button
@@ -64,101 +83,96 @@ const styles = StyleSheet.create({
     backgroundColor: "#D90504",
   },
   slide: {
-  
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#D90504",
   },
   container: {
-  
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#D90504",
   },
   topImagesContainer: {
-    position: 'relative',
+    position: "relative",
     flex: 1, // Occupy the entire available space
-  
   },
   centeredTopContainer: {
-    justifyContent: 'flex-start', 
-    alignItems: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   bgImage: {
-    width: windowWidth, 
+    width: windowWidth,
     height: windowHeight * 0.51,
     // Cover the image
   },
   centeredBottomContainer: {
     flex: 1,
-    
-    alignItems: 'center',
+
+    alignItems: "center",
   },
   centerImage: {
     width: windowWidth * 0.15,
     height: windowWidth * 0.15,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   bottomImageContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: -20,
-   
   },
   pinsImage: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     right: 30,
     width: windowWidth * 0.3,
     height: windowWidth * 0.3,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     zIndex: 1,
   },
   whiteCircle: {
     width: windowWidth * 0.35,
     height: windowWidth * 0.35,
     borderRadius: windowWidth * 0.2,
-    backgroundColor: '#FFF',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
     right: 60,
   },
   imagePin: {
     width: windowWidth * 0.15,
     height: windowWidth * 0.15,
     marginRight: -windowWidth * 0.15,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginBottom: -20,
-    
   },
   footerImage: {
     width: windowWidth * 0.3,
     height: windowWidth * 0.3,
-    resizeMode: 'contain',
-    position: 'relative',
-    left: 60, 
+    resizeMode: "contain",
+    position: "relative",
+    left: 60,
   },
   textContainer: {
-    position: 'relative',
+    position: "relative",
     right: 40,
+    width: 400,
     margin: 20,
     padding: 20,
-    paddingTop: 10, 
+    paddingTop: 10,
   },
 
   titleText: {
     fontSize: 24,
-    color: 'white',
-    textAlign: 'left',
+    color: "white",
+    padding: 20,
   },
   boldText: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   subTitleText: {
     fontSize: 16,
-    color: 'white',
-    textAlign: 'left',
-    paddingTop: 10, 
-   
+    color: "white",
+    padding: 20,
+    paddingTop: 10,
   },
 });
 
