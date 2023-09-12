d752328c0964024b0535e7e1676a71386c926d00
ChairmanT <taylor@dwebserver.net>
2023-09-12 23:01:04 +0200

    Pulled and resolved conflicts

--- src/screen/AppNavigator.tsx
+++ src/screen/AppNavigator.tsx
@@ -4,10 +4,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 import FuduOnboarding from "./LandingPage/FuduOnboarding";
 import LandingPage from "../screen/LandingPage/LandingPage";
 import HomePage from "./HomePage";
-//import LoginScreen from "./accounts/Login";
+//import LoginScreen from "./accounts/Login"
+import LoginScreen from "./accounts/Login";
 import SignupScreen from "./accounts/Signup";
 import PhoneVerificationScreen from "./accounts/PhoneVerication";
-import TextInANest from "./LandingPage/MyAccount";
+import AccountPage from "./LandingPage/MyAccount";
+import ForgottenPasswordScreen from "./accounts/ForgottenPassword";
+import ResetPasswordScreen from "./accounts/ResetPassword";
+import BottomTabNavigator from "./tabs/BottomTabNavigator";
+
 
 const Stack = createStackNavigator();
 
@@ -19,8 +24,12 @@ const AppNavigator: React.FC = () => {
       <Stack.Screen name="Home" component={HomePage} />
 
       <Stack.Screen name="Signup" component={SignupScreen} />
-      <Stack.Screen name="Myaccount" component={TextInANest} />
+      <Stack.Screen name="Myaccount" component={AccountPage} />
       {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+      {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Tabs" component={BottomTabNavigator} />
+
       <Stack.Screen
         name="Phone-verification"
         component={PhoneVerificationScreen}
--- src/screen/LandingPage/MyAccount.tsx
+++ src/screen/LandingPage/MyAccount.tsx
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  ActivityIndicator,
-  ImageBackground,
-} from "react-native";
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, ActivityIndicator, ImageBackground } from 'react-native';
 import { connect, useDispatch, useSelector } from "react-redux";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -14,36 +7,32 @@ const Stack = createStackNavigator();
 
 const AccountPage = () => {
   const [titleText, setTitleText] = useState("Delivered Fast at your Door");
-  const bodyText = "Contrary to popular belief borem text.";
+  const bodyText = 'Contrary to popular belief borem text.';
   const { isLoggedIn, error } = useSelector((state: any) => state?.auth);
+  
 
-  const handleLogin = () => {
-    // Validation logic
-
-    console.log("Login successful!");
-  };
+    const handleLogin = () => {
+        // Validation logic
+        
+    
+        console.log("Login successful!");
+    };
 
   return (
     <ImageBackground
-      source={require("../LandingPage/TopEdited.png")}
+      source={require('../LandingPage/TopEdited.png')}
       style={styles.background}
     >
-      <View style={styles.container}>
-        <Text style={styles.titleText}>
-          <Text style={styles.boldText}>Delivered Fast</Text> at {"\n"}
-          your <Text style={styles.boldText}>Door</Text>
-        </Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>
+        <Text style={styles.boldText}>Delivered Fast</Text> at {'\n'} 
+        your <Text style={styles.boldText}>Door</Text>
+      </Text>
 
-        <Text style={styles.baseText}>{bodyText}</Text>
-        <Text>It has roots clintock.</Text>
+      <Text style={styles.baseText}>{bodyText}</Text>
+      <Text>It has roots clintock.</Text>
+    
 
-        <TouchableOpacity style={styles.createAccount} onPress={handleLogin}>
-          {isLoggedIn ? (
-            <ActivityIndicator color="white" size="small" />
-          ) : (
-            <Text style={styles.createAccountText}>Create an account</Text>
-          )}
-        </TouchableOpacity>
 
       <TouchableOpacity style={styles.createAccount} onPress={handleLogin}>
         {isLoggedIn ? (
@@ -74,29 +63,29 @@ const AccountPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 200,
   },
   baseText: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 16,
-    fontFamily: "Roboto",
+    fontFamily: 'Roboto',
     marginTop: 15,
   },
 
   boldText: {
-    fontWeight: "bold",
-    fontFamily: "Roboto",
+    fontWeight: 'bold',
+    fontFamily: 'Roboto',
     marginTop: 100,
   },
 
-  titleText: {
+   titleText: {
     fontSize: 33,
-    textAlign: "center",
+    textAlign: 'center',
   },
 
   createAccount: {
-    backgroundColor: "#03A60F",
+    backgroundColor: '#03A60F',
     borderRadius: 32,
     width: "70%",
     marginTop: 120,
@@ -135,10 +124,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 
-  background: {
+   background: {
     flex: 1,
-    resizeMode: "cover",
+    resizeMode: 'cover',
   },
+  
 });
 
 
